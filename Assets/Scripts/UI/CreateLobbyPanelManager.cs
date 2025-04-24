caedf405c4db292bdcc5f5fd4ed0f81e87ae1292
Joel <joel.wolber@gmail.com>
2025-04-24 14:23:01 +0200

    added the fucking main menu

---
+++ Assets/Scripts/UI/CreateLobbyPanelManager.cs
@@ -0,0 +1,46 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.UI;
+
+public class CreateLobbyPanelManager : MonoBehaviour
+{
+    public TMP_InputField lobbyNameInput;
+
+    public Button createLobbyButton;
+
+    void Start()
+    {
+        lobbyNameInput.onValueChanged.AddListener(LobbyNameInput_TMP_ValueChanged);
+        DisableCreateLobbyButton();
+    }
+
+    void LobbyNameInput_TMP_ValueChanged(string newValue)
+    {
+        if (newValue != "Enter Lobby Name...".Trim() && !string.IsNullOrEmpty(newValue))
+        {
+            createLobbyButton.interactable = true;
+            Debug.Log("Enable Create Lobby Button");
+        }
+    }
+
+    public void CreateLobby()
+    {
+        string enteredName = lobbyNameInput.text;
+
+        MainMenuManager.Instance.lobbyName = enteredName;
+        Debug.Log("Saved Lobbyname: " + enteredName);
+    }
+
+    public void ResetLobbyName()
+    {
+        Debug.Log("Reset Lobbyname");
+        lobbyNameInput.text = "Enter Lobbyname...";
+        DisableCreateLobbyButton();
+    }
+
+    private void DisableCreateLobbyButton()
+    {
+        createLobbyButton.interactable = false;
+        Debug.Log("Disable Create Lobby Button");
+    }
+}
---
+++ Assets/Scripts/UI/LobbyPanelManager.cs
@@ -0,0 +1,52 @@
+using UnityEngine;
+using TMPro;
+using UnityEngine.SceneManagement;
+
+public class LobbyPanelManager : MonoBehaviour
+{
+    public TMP_Text lobbyNameTMP;
+
+    public TMP_Text lobbyPortTMP;
+
+    public TMP_Text lobbyIPTMP;
+
+    public void initiateLobby()
+    {
+        SetLobbyName();
+        SetLobbyPortIPTMP();
+    }
+
+    private void SetLobbyName()
+    {
+        string lobbyName = MainMenuManager.Instance.lobbyName;
+        Debug.Log($"Set Lobbyname to {lobbyName}" );
+        lobbyNameTMP.text = "Lobby: " + lobbyName;
+    }
+
+    private void SetLobbyPortIPTMP()
+    {
+        string lobbyIP = MainMenuManager.Instance.lobbyIP;
+        string lobbyPort = MainMenuManager.Instance.lobbyPort;
+
+        if (!string.IsNullOrEmpty(lobbyPort))
+        {
+            lobbyPortTMP.text = "Port: " + lobbyPort;
+
+        }
+
+        Debug.Log("Set Lobby Port: " + lobbyPort + " Lobby IP: " + lobbyIP);
+        lobbyIPTMP.text = "IP: " + lobbyIP;
+    }
+
+    public void ResetLobbyTMPs()
+    {
+        lobbyNameTMP.text = string.Empty;
+        lobbyPortTMP.text = string.Empty;
+        lobbyIPTMP.text = string.Empty;
+    }
+
+    public void StartGame()
+    {
+        SceneManager.LoadScene("Game");
+    }
+}
