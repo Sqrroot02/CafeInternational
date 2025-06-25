472fbfa56d2a6bc412c3b8b35c305d4be69dd102
Joel <joel.wolber@gmail.com>
2025-06-25 20:17:42 +0200

    unit test

---
+++ Assets/Scripts/UI/Tests/MainMenuHelperTest.cs
@@ -0,0 +1,14 @@
+using NUnit.Framework;
+
+public class MainMenuHelperTests
+{
+    //[TestCase("Alice", ExpectedResult = true)]
+    //[TestCase("Max123", ExpectedResult = false)]
+    //[TestCase("", ExpectedResult = false)]
+    //[TestCase("Anna!", ExpectedResult = false)]
+    //[TestCase("ABCDEFGHIJKLMNO", ExpectedResult = true)]
+    //public bool User_or_Lobbyname_is_valid_ut(string input)
+    //{
+    //    return MainMenuHelper.IsValidNicknameOrLobbyName(input);
+    //}
+}
